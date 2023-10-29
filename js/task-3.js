0621b8f27fcf4ce72140e6cd2a2ff394cb557ce2
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2023-10-29 22:38:56 +0000

    task-3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function checkForSpam(message) {
+  message = message.toLowerCase();
+  const spam = 'spam';
+  const sale = 'sale';
+  if (message.includes(spam)) {
+    return true;
+  } else if (message.includes(sale)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
